ec50ae3b3b070c864ac7988f9454f55983b6694d
Derek Felson <drfelson@gmail.com>
2016-03-24 13:14:22 -0400

    Tests for min and max entropy

--- tests/DecisionTreeTests.cpp
+++ tests/DecisionTreeTests.cpp
@@ -16,19 +16,37 @@ TEST(DecisionTreeTests, EntropyZeroWithOnlyOneClass)
 	DataMatrix testData2{5, 3};
 	TypeVector testTypes2{5, 1};
 
-	testData2(0,0) = 5.0;
-	testData2(0,1) = 4.0;
-	testData2(0,2) = 3.0;
-	testData2(1,0) = 5.0;
-	testData2(1,1) = 4.0;
-	testData2(1,2) = 3.0;
-	testData2(2,0) = 5.0;
-	testData2(2,1) = 4.0;
-	testData2(2,2) = 3.0;
-	testTypes2[0] = 7;
-	testTypes2[1] = 7;
-	testTypes2[2] = 7;
+	testData2 << 5, 4, 3,
+			     5, 4, 3,
+				 5, 4, 3,
+				 5, 4, 3,
+				 5, 4, 3;
+
+	testTypes2 << 7, 7, 7, 7, 7;
 
 	EXPECT_EQ(0, entropy(testData1, testTypes1));
 	EXPECT_EQ(0, entropy(testData2, testTypes2));
+}
+
+TEST(DecisionTreeTests, MaxEntropy)
+{
+	DataMatrix testData1{2, 1};
+	TypeVector testTypes1{2, 1};
+
+	testData1 << 5, 4;
+	testTypes1 << 1, 2;
+
+	DataMatrix testData2{5, 3};
+	TypeVector testTypes2{5, 1};
+
+	testData2 << 5, 4, 3,
+			     5, 4, 3,
+				 5, 4, 3,
+				 5, 4, 3,
+				 5, 4, 3;
+
+	testTypes2 << 1, 2, 3, 2, 1;
+
+	EXPECT_EQ(1, entropy(testData1, testTypes1));
+	EXPECT_EQ(1, entropy(testData2, testTypes2));
 }
