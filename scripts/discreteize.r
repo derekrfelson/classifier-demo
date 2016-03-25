26a7d7e0ff16f1ab9a4459c142acd3619f7fc6d6
Derek Felson <drfelson@gmail.com>
2016-03-25 15:29:32 -0400

    Removed quotation marks because they messed up the parse

--- scripts/discreteize.r
+++ scripts/discreteize.r
@@ -1,14 +1,14 @@
-install.packages("arules")
+#install.packages("arules")
 library(arules)
 
 x <- read.csv("../data/wine.csv", header=FALSE)
-for (i in 1:13) { x[,i] = discretize(x[,i], categories=4, labels=c('1', '2', '3', '4')) }
-write.table(x, file="../data/wineDiscrete.csv", row.names=FALSE, na="", col.names=FALSE, sep=",")
+for (i in 1:13) { x[,i] = discretize(x[,i], categories=4, labels=c(1, 2, 3, 4)) }
+write.table(x, file="../data/wineDiscrete.csv", row.names=FALSE, quote=FALSE, na="", col.names=FALSE, sep=",")
 
 x <- read.csv("../data/iris.csv", header=FALSE)
-for (i in 1:4) { x[,i] = discretize(x[,i], categories=4, labels=c('1', '2', '3', '4')) }
-write.table(x, file="../data/irisDiscrete.csv", row.names=FALSE, na="", col.names=FALSE, sep=",")
+for (i in 1:4) { x[,i] = discretize(x[,i], categories=4, labels=c(1, 2, 3, 4)) }
+write.table(x, file="../data/irisDiscrete.csv", row.names=FALSE, quote=FALSE, na="", col.names=FALSE, sep=",")
 
 x <- read.csv("../data/heartDisease.csv", header=FALSE)
-for (i in c(1,4,5,8,10)) { x[,i] = discretize(x[,i], categories=4, labels=c('1', '2', '3', '4')) }
-write.table(x, file="../data/heartDiseaseDiscrete.csv", row.names=FALSE, na="", col.names=FALSE, sep=",")
+for (i in c(1,4,5,8,10)) { x[,i] = discretize(x[,i], categories=4, labels=c(1, 2, 3, 4)) }
+write.table(x, file="../data/heartDiseaseDiscrete.csv", row.names=FALSE, quote=FALSE, na="", col.names=FALSE, sep=",")
