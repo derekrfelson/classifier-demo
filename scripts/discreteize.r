7671f7b540d2562efb46bd69045f8c44e87e812f
Derek Felson <drfelson@gmail.com>
2016-03-27 13:48:03 -0400

    Optimal discretization happens with 3 categories

--- scripts/discreteize.r
+++ scripts/discreteize.r
@@ -1,14 +1,17 @@
 #install.packages("arules")
 library(arules)
 
+numcols <- 3;
+clabels <- c(1, 2, 3)
+
 x <- read.csv("../data/wine.csv", header=FALSE)
-for (i in 1:13) { x[,i] = discretize(x[,i], categories=4, labels=c(1, 2, 3, 4)) }
+for (i in 1:13) { x[,i] = discretize(x[,i], categories=numcols, labels=clabels) }
 write.table(x, file="../data/wineDiscrete.csv", row.names=FALSE, quote=FALSE, na="", col.names=FALSE, sep=",")
 
 x <- read.csv("../data/iris.csv", header=FALSE)
-for (i in 1:4) { x[,i] = discretize(x[,i], categories=4, labels=c(1, 2, 3, 4)) }
+for (i in 1:4) { x[,i] = discretize(x[,i], categories=numcols, labels=clabels) }
 write.table(x, file="../data/irisDiscrete.csv", row.names=FALSE, quote=FALSE, na="", col.names=FALSE, sep=",")
 
 x <- read.csv("../data/heartDisease.csv", header=FALSE)
-for (i in c(1,4,5,8,10)) { x[,i] = discretize(x[,i], categories=2, labels=c(1, 2)) }
+for (i in c(1,4,5,8,10)) { x[,i] = discretize(x[,i], categories=numcols, labels=clabels) }
 write.table(x, file="../data/heartDiseaseDiscrete.csv", row.names=FALSE, quote=FALSE, na="", col.names=FALSE, sep=",")
